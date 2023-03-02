40f2cf406e36346a6aba45b457f4daab60d06744
jaganjavid <jagantutor1@gmail.com>
2023-03-02 19:34:41 +0530

    upto object pro

--- 5-object-project/app.js
+++ 5-object-project/app.js
@@ -22,7 +22,7 @@ function UI(){
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td>X</td>
+        <td class="delete">X</td>
     `;
 
     list.appendChild(row);
@@ -32,6 +32,38 @@ function UI(){
         document.querySelector("#bookname").value = "";
         document.querySelector("#author").value = "";
         document.querySelector("#isbn").value = "";
+    }
+
+    UI.prototype.showAlert = function(message,className){
+    //   Create Div
+    
+    const div = document.createElement("div");
+    // Add Class to div
+    div.className = `alert ${className}`;
+
+    // Add Text 
+    div.appendChild(document.createTextNode(message));
+
+    // Get Container
+    const container = document.querySelector(".container");
+
+    // Get form
+    const form = document.querySelector("#book-form");
+
+    // Insert Alert 
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector(".alert").remove();
+    }, 3000)
+
+    }
+    
+    UI.prototype.deleteBook = function(target){
+        if(target.className === "delete"){
+            target.parentElement.remove();
+        }
     }
 }
 
@@ -54,10 +86,83 @@ document.getElementById("book-form").addEventListener("submit", function(e){
 
     if(title === "" || author === "" || isbn === ""){
         //  Alert
-        alert("Please fill the form");
+        ui.showAlert("Please Fill the form", "error")
     } else {
         ui.addBookToList(book);
+        ui.showAlert("Book Added", "success");
         ui.clearFields();
+        
     }
     
 })
+
+// Event listener for delete
+
+document.getElementById("book-list").addEventListener("click", function(e){
+    e.preventDefault();
+
+    if(e.target.className === "delete"){
+        // Instantiate book
+        const ui = new UI();
+
+        ui.deleteBook(e.target);
+
+        ui.showAlert("Book Deleted", "success")
+    }
+})
+
+
+// setTimeout(function(){
+//     console.log("Hai")
+// }, 3000);
+
+
+
+
+
+
+// UI.prototype.showAlert = function(message,className){
+//     //   Create Div
+    
+//     const div = document.createElement("div");
+//     // Add Class to div
+//     div.className = `alert ${className}`;
+
+//     // Add a style to based on classes
+
+//     if(div.classList.contains("alert")){
+//         div.style.marginBottom = "20px";
+//         div.style.padding = "5px";
+//         div.style.background = "#ffffff";
+//     }
+
+//     if(div.classList.contains("success")){
+//         div.style.background = "green";
+//     } else {
+//         div.style.background = "red";
+//     }
+
+//     // Add Text 
+//     div.appendChild(document.createTextNode(message));
+
+//     // Get Container
+//     const container = document.querySelector(".container");
+
+//     // Get form
+//     const form = document.querySelector("#book-form");
+
+//     // Insert Alert 
+//     container.insertBefore(div, form);
+
+//     // Timeout after 3 sec
+//     setTimeout(function(){
+//         document.querySelector(".alert").remove();
+//     }, 3000)
+
+//     }
+    
+//     UI.prototype.deleteBook = function(target){
+//         if(target.className === "delete"){
+//             target.parentElement.remove();
+//         }
+//     }
