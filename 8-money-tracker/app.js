c0ff914aaae1de32f1660815c67987b88c063c46
jaganjavid <jagantutor1@gmail.com>
2023-03-29 20:00:39 +0530

    add item to list

--- 8-money-tracker/app.js
+++ 8-money-tracker/app.js
@@ -8,6 +8,14 @@
 // Item Controller
 const itemCtrl = (function(){
 
+    // Item Constructor
+
+    const Item = function(id, name, money){
+        this.id = id;
+        this.name = name;
+        this.money = money;
+    }
+
 
     // Data Structure / State
 
@@ -15,7 +23,7 @@ const itemCtrl = (function(){
         items: [
             {id:0, name: "Bike", money:3000},
             {id:1, name: "Phone", money:1500},
-            {id:0, name: "Clothes", money:800},
+            {id:2, name: "Clothes", money:800},
         ],
         totalMoney: 0,
         currentItem: null
@@ -24,6 +32,26 @@ const itemCtrl = (function(){
     return {
         getItem: function(){
             return data.items;
+        },
+        addItem:function(name, money){
+            let ID;
+            // Create ID
+            if(data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1;
+                console.log(ID);
+            } else {
+                ID = 0;
+            }
+
+            money = parseInt(money);
+
+            // Create new Item
+            newItem = new Item(ID, name, money);
+
+            // Add to item array
+            data.items.push(newItem);
+
+            return newItem;
         }
     }
 
@@ -35,22 +63,120 @@ const itemCtrl = (function(){
 
 
 // UI Controller
-// const UICtrl = (function(){
+const UICtrl = (function(){
 
-// }())
+    const UISelectors = {
+        itemList: "#item-list",
+        addBtn: ".add-btn",
+        updateBtn: ".update-btn",
+        deleteBtn: ".delete-btn",
+        backBtn: ".back-btn",
+        inputItem:"item-name",
+        inputMoney: "item-money"
+    }
+
+    return {
+        populateItemList:function(items){
+          let html = "";
+
+          items.forEach(function(item){
+            html += `
+            <li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}:</strong> <em>${item.money}$</em>
+                <a href="#" class="secondary-content">
+                    <i class="fa fa-pencil"></i>
+                </a>
+            </li>
+            `;
+          });
+
+        //   Insert List Items
+        document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        clearEditState: function(){
+            document.querySelector(UISelectors.addBtn).style.display = "inline";
+            document.querySelector(UISelectors.updateBtn).style.display = "none";
+            document.querySelector(UISelectors.deleteBtn).style.display = "none";
+            document.querySelector(UISelectors.backBtn).style.display = "none";
+        },
+        getSelectors: function(){
+            return UISelectors;
+        },
+        getItemInput: function(){
+            const name = document.getElementById(UISelectors.inputItem).value;
+            const money = document.getElementById(UISelectors.inputMoney).value;
+            return {
+                name:name,
+                money: money
+            }
+        },
+        addListItem: function(item){
+            
+            // Create ali
+            const li = document.createElement("li");
+
+            // Add Class
+            li.className = "collection-item";
+
+            // Add ID
+            li.id = `item-${item.id}`;
+
+            // Add HTML
+            li.innerHTML = `<strong>${item.name}</strong> <em>${item.money}$</em>
+                <a href="#" class="secondary-content">
+                    <i class="fa fa-pencil"></i>
+                </a>
+            </li>`;
+
+            // Insert to ul
+            document.querySelector(UISelectors.itemList).appendChild(li);
+        }
+    }
+}())
+
+
 
 
 // App Controller
 
-const App = (function(itemCtrl){
-  return {
-    init: function(){
-        const items = itemCtrl.getItem();
+const App = (function(itemCtrl,UICtrl){
 
-        console.log(items);
+ const selectors = UICtrl.getSelectors(); 
+
+  const loadEventListeners = function(){
+    // Add Item Event
+    document.querySelector(selectors.addBtn).addEventListener("click", itemAddSubmit);
+  }  
+
+  const itemAddSubmit = function(e){
+    e.preventDefault();
+    
+    const input = UICtrl.getItemInput();
+
+    if(input.name !== "" && input.money !== ""){
+      // Add Item
+     const newItem = itemCtrl.addItem(input.name,input.money);
+
+     UICtrl.addListItem(newItem);
     }
   }
-}(itemCtrl))
+
+  return {
+    init: function(){
+        UICtrl.clearEditState();
+        const items = itemCtrl.getItem();
+
+        if(items.length === 0){
+            console.log("There is no item")
+        } else {
+            // Populate list with items
+            UICtrl.populateItemList(items);
+        }
+
+        loadEventListeners();
+    }
+  }
+}(itemCtrl,UICtrl))
 
 // Intitialize App
 
